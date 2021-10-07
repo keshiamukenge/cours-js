bbd880f68dd1136b7ee3d9150b07a8aa3d75f7bf
keshiamukenge <keshia_mkg@live.fr>
2021-10-07 09:12:16 +0200

    add syntax - part 1 - correction

--- exos/2_syntax/part-1.js
+++ exos/2_syntax/part-1.js
@@ -8,12 +8,12 @@ console.log("--- Bienvenue sur l'autoroute A7 ---");
 console.log('--- 1 ---');
 // créer une variable x, et lui assigner un nombre
 
-const x = 34;
+const x = 5;
 
 console.log('--- 2 ---');
 // logguer cette variable dans la console avec console.log()
 
-console.log('X', x);
+console.log(x);
 
 console.log('--- 3 ---');
 /*
@@ -22,8 +22,8 @@ en utilisant une boucle for.
 */
 const speeds = [23, 12, 57, 178, 129, 434, 222, 1000, 59, 887, 134];
 
-for (let i = 0; i < speeds.length; i++) {
-  console.log('Speed', speeds[i]);
+for(let i = 0; i < speeds.length; i++){
+  console.log(speeds[i]);
 }
 
 console.log('--- 4 ---');
@@ -36,18 +36,20 @@ function moins5(nb) {
   return nb - 5;
 }
 
+console.log(moins5(x));
+
 console.log('--- 5 ---');
 /*
   changer la boucle de la question 3 pour corriger chaque nombre avec la fonction 'moins5'.
   Si le résultat est supérieur à 130, logguer 'Vous allez trop vite !'
 */
 
-for (let i = 0; i < speeds.length; i++) {
-  const correction = moins5(speeds[i]);
-  console.log('Vitesse', speeds[i], ', corrigée', correction);
-
-  if (correction > 130) {
-    console.log('Ralentissez !');
+for(let i = 0; i < speeds.length; i++){
+  const value = moins5(speeds[i]);
+  if(value > 130) {
+    console.log(value, 'Vous allez trop vite');
+  } else {
+    console.log(value);
   }
 }
 
@@ -61,33 +63,12 @@ console.log('--- 1 ---');
   ( Utiliser Math.random() et Math.floor() )
 */
 
-function getNumber(limit) {
-  const between0And1 = Math.random();
-  const randomNb = between0And1 * limit;
-
-  const randomInt = Math.floor(randomNb);
-
-  return randomInt;
-}
-
-console.log('Un nb aléatoire entre 0 et ', 19, '=>', getNumber(19));
-
 console.log('--- 2 ---');
 /*
 créer une fonction 'getLetter' qui renvoie une lettre au hasard parmi le tableau 'letters'.
 */
 
 const letters = ['abcdefghijklmnopqrstuvwxyz'];
-
-function getLetter() {
-  const position = getNumber(letters[0].length);
-
-  const letter = letters[0][position];
-
-  return letter;
-}
-
-console.log('Une lettre aléatoire ', getLetter());
 
 console.log('--- 3 ---');
 /*
@@ -96,18 +77,6 @@ console.log('--- 3 ---');
   Utiliser la fonction 'getNumber', 'getLetter' ainsi qu'une boucle for.
 */
 
-function makePassword(size) {
-  let password = '';
-
-  for (let i = 0; i < size; i++) {
-    password = password + getLetter();
-  }
-
-  return password;
-}
-
-console.log('Mon mdp:', makePassword(12));
-
 console.log('--- bonus ---');
 /*
   créer une fonction 'makeStrongerPassword' qui en entrée prend un nombre 'size' et un booléen 'withNumbers'
@@ -115,31 +84,6 @@ console.log('--- bonus ---');
   - Si 'withNumber' est vrai, faire en sorte qu'un caractère sur deux soit un nombre au hasard.
   - Si 'size' est plus petit que 8, logguer un message d'avertissement, mais créer le password quand même.
 */
-
-function makeStrongerPassword(size, withNumber) {
-  let password = '';
-
-  for (let i = 0; i < size; i++) {
-    if (withNumber) {
-      if (i % 2 === 0) {
-        password = password + getLetter();
-      } else {
-        password = password + getNumber(10);
-      }
-    } else {
-      password = password + getLetter();
-    }
-  }
-
-  if (password.length < 8) {
-    console.log("c'est de la merde ton pwd");
-  }
-
-  return password;
-}
-
-console.log('Mon meilleur mdp ', makeStrongerPassword(16, true));
-console.log('Mon meilleur mdp ', makeStrongerPassword(4, false));
 
 console.log('');
 console.log('--- Dis bonjour à mamie ---');
@@ -150,34 +94,18 @@ console.log('--- 1 ---');
   Utiliser cette fonction avec votre prénom en logguant son résultat
 */
 
-function bonjour(name) {
-  return 'Bonjour ' + name;
-}
-console.log(bonjour('Clemence'));
+
 console.log('--- 2 ---');
 /*
   sur le même principe, créer une fonction 'bonsoir'
 */
-function bonsoir(name) {
-  return 'Bonsoir ' + name;
-}
-console.log(bonsoir('Clemence'));
+
 console.log('--- 3 ---');
 /*
   créer une fonction 'saluer', qui en fonction de l'heure, utilise 'bonjour' ou 'bonsoir'.
   Utiliser new Date().getHours() pour avoir l'heure actuelle.
 */
-function saluer() {
-  const hour = new Date().getHours();
-  if (hour > 18) {
-    console.log(bonsoir('Clemence'));
-  } else {
-    console.log(bonjour('Clemence'));
-  }
-}
 
-const result = saluer();
-console.log('resulatst', result);
 console.log('--- 4 ---');
 /*
 en utilisant le tableau de langues 'languages' ainsi qu'une boucle for,
@@ -185,21 +113,6 @@ dire bonjour dans toutes les langues disponibles
 */
 const languages = ['fr', 'es', 'de', 'it', 'jp'];
 
-for (let i = 0; i < languages.length; i++) {
-  let langue = languages[i];
-  if (langue === 'fr') {
-    console.log('Bonjour');
-  } else if (langue === 'es') {
-    console.log('Hola');
-  } else if (langue === 'de') {
-    console.log('Hallo');
-  } else if (langue === 'it') {
-    console.log('Ciao');
-  } else if (langue === 'jp') {
-    console.log('Konnichiwa');
-  }
-}
-console.log('--- bonus ---');
 /*
   créer une fonction 'prepareHello' qui prend en entrée une langue 'lang' ('fr', 'it', 'es', 'de', ...)
   et qui, en fonction de la langue, renvoie une fonction anonyme qui prend en entrée un 'name',
@@ -207,40 +120,3 @@ console.log('--- bonus ---');
   Si on ne passe pas de langue, ou si la langue n'est pas reconnue, l'anglais doit être le défaut.
   Utiliser 'prepareHello' pour créer plusieurs fonctions pour dire bonjour dans plusieurs langues, et les essayer.
 */
-
-function prepareHello(lang) {
-  if (lang === 'fr') {
-    return function (name) {
-      return 'Bonjour ' + name;
-    };
-  } else if (lang === 'es') {
-    return function (name) {
-      return 'Hola ' + name;
-    };
-  } else if (lang === 'it') {
-    return function (name) {
-      return 'Ciao ' + name;
-    };
-  } else if (lang === 'de') {
-    return function (name) {
-      return 'Hallo ' + name;
-    };
-  } else if (lang === 'jp') {
-    return function (name) {
-      return 'Konnichiwa ' + name;
-    };
-  } else {
-    return function (name) {
-      return 'Hello ' + name;
-    };
-  }
-}
-
-const helloFr = prepareHello('fr');
-const helloIt = prepareHello('it');
-const helloRu = prepareHello('ru');
-
-console.log(helloFr('Jacky'));
-console.log(helloFr('Nicole'));
-console.log(helloIt('Jacky'));
-console.log(helloRu('Jacky'));
